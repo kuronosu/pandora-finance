7b520a653fad0c2eba017a469ddcc898fb613b3f
Andres Felipe Martinez Salazar <andresfelipe.2031@gmail.com>
2020-07-24 01:57:30 -0500

    set debug to true

--- pandora_finance/settings.py
+++ pandora_finance/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0r)n-nvq%qh)$pqsvxd1#-4*$qrxal7-=t=p*b89n@dtemo!6u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost',
                  '.herokuapp.com', 'pandora.kuronosu.dev']
